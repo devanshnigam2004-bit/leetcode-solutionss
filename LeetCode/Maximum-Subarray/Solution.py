5f694c0359101ac33e6c5b7fea53a6984a23dc01
devanshnigam2004-bit <devanshnigam2004@gmail.com>
2026-06-03 09:24:37 +0530

    Add solution for Maximum Subarray

--- LeetCode/Maximum-Subarray/Solution.py
+++ LeetCode/Maximum-Subarray/Solution.py
@@ -1,10 +1,15 @@
 1class Solution:
-2    def maxSubArray(self, nums):
-3        current_sum = nums[0]
-4        max_sum = nums[0]
-5        
-6        for i in range(1, len(nums)):
-7            current_sum = max(nums[i], current_sum + nums[i])
-8            max_sum = max(max_sum, current_sum)
-9        
-10        return max_sum
+2    def maxSubArray(self, nums: list[int]) -> int:
+3        left = 0
+4        current = 0
+5        max_sum = float('-inf')
+6
+7        for right in range(len(nums)):
+8            current += nums[right]
+9            max_sum = max(max_sum, current)
+10
+11            if current < 0:
+12                current = 0
+13                left = right + 1
+14
+15        return max_sum
