cf00fd4397ec9ab0167b132dd28ae6e0c9fbaf92
devanshnigam2004-bit <devanshnigam2004@gmail.com>
2026-06-06 09:39:53 +0530

    Add solution for Image Smoother

---
+++ LeetCode/Image-Smoother/Solution.py
@@ -0,0 +1,20 @@
+1class Solution:
+2    def imageSmoother(self, img: list[list[int]]) -> list[list[int]]:
+3        m, n = len(img), len(img[0])
+4        result = [[0] * n for _ in range(m)]
+5
+6        for i in range(m):
+7            for j in range(n):
+8                total = 0
+9                count = 0
+10
+11                for di in [-1, 0, 1]:
+12                    for dj in [-1, 0, 1]:
+13                        ni, nj = i + di, j + dj
+14                        if 0 <= ni < m and 0 <= nj < n:
+15                            total += img[ni][nj]
+16                            count += 1
+17
+18                result[i][j] = total // count
+19
+20        return result
