f1917c4940ebbbc390216195d86a8ec031200b3c
devanshnigam2004-bit <devanshnigam2004@gmail.com>
2026-06-06 09:09:04 +0530

    Add solution for Missing Number

---
+++ LeetCode/Missing-Number/Solution.py
@@ -0,0 +1,6 @@
+1class Solution:
+2    def missingNumber(self, nums: list[int]) -> int:
+3        n = len(nums)
+4        expected = n * (n + 1) // 2
+5        return expected - sum(nums)
+6        
